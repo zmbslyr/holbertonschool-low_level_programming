b96798dc768cc6a252c68501d87b2c0bfdc86ea0
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-06 19:32:05 +0000

    Lists sizes of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", 1);
+	printf("Size of an int: %d byte(s)\n", 4);
+	printf("Size of a long int: %d byte(s)\n", 4);
+	printf("Size of a long long int: %d byte(s)\n", 8);
+	printf("Size of a float: %d byte(s)\n", 4);
+	return (0);
+}

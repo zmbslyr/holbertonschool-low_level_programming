e7467f417310e0bcf0ca654cb54f4f235b69d5bf
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-12 05:17:39 +0000

    checks if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks if character is lower case
+ *
+ * Return: 1 (Sucess)
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	return (0);
+}

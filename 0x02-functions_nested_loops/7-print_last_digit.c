887804e6ead55a1da4e62042cd137678e6808ae8
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-12 19:17:15 +0000

    Prints last digit of input number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: integer
+ *
+ * Return: last digit of a number
+ */
+int print_last_digit(int i)
+{
+	int n;
+
+	if (i >= 0)
+	{
+		n = i % 10;
+	}
+	else
+	{
+		n = -i % 10;
+	}
+	_putchar (n + '0');
+	return(n);
+}

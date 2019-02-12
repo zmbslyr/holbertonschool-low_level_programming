8175c185173c3482ab9854b596ad910a68364fda
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-12 04:28:44 +0000

    Prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}

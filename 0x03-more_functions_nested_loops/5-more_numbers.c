ef83eab2d0415c07b788c42e1be750f35b0af9ef
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-14 22:20:42 +0000

    counts to 14 10 times

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints ten sets of 0-14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

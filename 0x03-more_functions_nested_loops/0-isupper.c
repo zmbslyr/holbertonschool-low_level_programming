239f5cebed9b92477d3bdccb67eef1ae92f4433c
zmbslyr <mhedgeland@gmail.com>
2019-02-14 20:01:05 +0000

    Checks if a character is upper case

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Determines if a character is upper case
+ * @c: character
+ *
+ * Return: 1 if uppercase, 0 is anything else
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}

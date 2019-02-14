154855f9c3d49b64accc69780e16d21caf7d269d
zmbslyr <mhedgeland@gmail.com>
2019-02-14 21:27:42 +0000

    Prints most numbers

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints all numbers from 0-9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+        int x = 0;
+
+        while (x >= 0 && x < 10)
+        {
+                if (x != 2)
+                {
+			if (x != 4)
+			{
+				_putchar(x + '0');
+			}
+                }
+                x++;
+        }
+        _putchar('\n');
+}

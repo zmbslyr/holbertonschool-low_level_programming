8c83ffcc9b42d510f40c53b9c4f8c6c20b3c2e32
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-15 03:54:04 +0000

    prints a square

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - Prints a square
+ * @size: size of square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar('#');
+		}
+		putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}

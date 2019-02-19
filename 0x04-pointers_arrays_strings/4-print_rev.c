bfe9e61e44f0c07de8bdee23ce299bf735718f01
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-19 21:07:22 +0000

    update

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,17 @@
  */
 void print_rev(char *s)
 {
-	int x;
+	int x, y;
 	char l;
 
 	for (x = 0; s[x] != 0; x++)
 	{
 
 	}
+	y = x;
 	for (; s[x] >= 0; x--)
 	{
-		if (s[x] != '\0')
+		if (x != y)
 		{
 			l = s[x];
 			_putchar(l);

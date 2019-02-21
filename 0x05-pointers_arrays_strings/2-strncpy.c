3b11c8578690006afffd518dfcd71f1f6559b50e
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-21 19:42:01 +0000

    Copies a string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer to destination array
+ * @src: Pointer to string to put in array
+ * @n: max number of bytes
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	x = 0;
+	while (src[x] != 0 && x < n)
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+	return(dest);
+}

81a7eb4e63e1d40e88b30a56e5a1a5de3138a0de
zmbslyr <mhedgeland@gmail.com>
2019-02-25 13:03:13 +0000

    Locates a character in a string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String to be searched
+ * @c: Character to look for in string
+ *
+ * Return: Pointer to first occurance of character
+ */
+char *_strchr(char *s, char c)
+{
+	char *pointerS = s;
+
+	while (*pointerS != '\0')
+	{
+		if (*pointerS == c)
+		{
+			return (pointerS);
+			break;
+		}
+		pointerS++;
+	}
+	return (0);
+}

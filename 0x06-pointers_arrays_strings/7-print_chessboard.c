47f8a27f45c9eed416c8efbaaea0c813c5d251e0
zmbslyr <mhedgeland@gmail.com>
2019-02-25 23:38:19 +0000

    Prints chessboard

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: takes in array
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}

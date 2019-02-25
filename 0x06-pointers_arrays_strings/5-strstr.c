aa2adad0ff6928181e5fb96bb6efb7158f3ffb17
zmbslyr <mhedgeland@gmail.com>
2019-02-25 23:29:03 +0000

    Locates a substring

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds a sub-string in a string
+ * @haystack: String to search through
+ * @needle: Sub-string to search for
+ *
+ * Return: Beginning of sub-string in string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x, y, z;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		for (y = 0, z = x; needle[y] != '\0'; y++, z++)
+		{
+			if (haystack[z] != needle[y] || haystack[z] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[y] == '\0')
+		{
+			return(haystack + x);
+		}
+	}
+	return('\0');
+}

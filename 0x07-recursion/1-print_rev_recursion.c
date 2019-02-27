a276eeffcca16e8c872fdc3abc945c36d56f9783
Mark Hedgeland <mhedgeland@gmail.com>
2019-02-27 18:43:25 +0000

    Prints the reverse of a string

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a reverse string with recursion
+ * @s: input string to reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s* != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}

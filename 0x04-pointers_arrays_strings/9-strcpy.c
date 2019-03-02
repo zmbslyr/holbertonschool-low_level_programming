b83bb77c84ec5af4684470466df5d21c45d88225
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-02 01:39:55 +0000

    Copies a string

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: Array to put src in
+ * @src: String to copy to dest
+ *
+ * Return: Pointer to string dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x + 1] = '\0';
+	return (dest);
+}

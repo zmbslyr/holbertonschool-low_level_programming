cf7782df0062b26a3dc0824c8b3be05b8735a1d8
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-02 02:14:05 +0000

    Revision back to original structure

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -9,29 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int x, len;
+	int x;
 
-	len = _strlen(src);
-	for (x = 0; src[x] < len; x++)
+	for (x = 0; src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
 	}
+	dest[x] = '\0';
 	return (dest);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: Input string to be counted
- * 
- * Return: Length of a string
- */
-int _strlen(char *s)
-{
-	int x = 0;
-
-	while (s[x] != '\0')
-	{
-		x++;
-	}
-	return (x);
 }

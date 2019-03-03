c5742f30b68d2532ba3ff1b53f600065eb5e10fc
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-03 10:45:38 -0500

    Prints number of arguments given

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of command line arguments given
+ * @argc: Number of arguments given
+ * @argv: Array of string values of arguments
+ * 
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    if (argc > 0)
+    {
+        printf("%d\n", argc - 1);
+    }
+}

fda50c9646b047185bdb2888df033d1621d3801f
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-03 15:29:09 +0000

    Initial Commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints programs name
+ * @argc: Amount of command line arguments given
+ * @argv: Array holding string value of arguments given
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
---
+++ 0x09-argc_argv/README.md
@@ -0,0 +1,2 @@
+<h1> Read Me </h1>
+This is a progect about argc and argv[]

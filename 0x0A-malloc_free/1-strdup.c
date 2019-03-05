c7629f28e147224fecba24d3f8c3b0f695b10bbd
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-05 16:19:50 +0000

    Duplicates a string

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: String to be duplicated
+ *
+ * Return: Pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int index, size;
+	char *newArray;
+
+	size = _strlen(str);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	newArray = malloc(sizeof(*str) * size);
+	for (index = 0; index <= size; index ++)
+	{
+		newArray[index] = str[index];
+	}
+	return (newArray);
+	free(newArray);
+}
+
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find length of
+ *
+ * Return: Length of a string
+ */
+int _strlen(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+	}
+	return (index);
+}

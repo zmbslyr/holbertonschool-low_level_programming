0573fab41a2f35be2cb4913d4b05081f68e4b4a1
Mark Hedgeland <mhedgeland@gmail.com>
2019-03-07 21:51:50 +0000

    Betty Styling

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,27 +16,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concatStr, *newStringPointer, *s1Pointer, *s2Pointer;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	s1Pointer = s1;
 	s2Pointer = s2;
 	for (; *s2Pointer; s2Pointer++)
-	{
 		lens2++;
-	}
 	for (; *s1Pointer; s1Pointer++)
-	{
 		totLen++;
-	}
 	if (n > lens2)
-	{
 		totLen += lens2;
-	}
 	else
 	{
 		for (; index < n; index++)
@@ -47,13 +37,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concatStr = malloc(sizeof(char) * totLen + 1);
 	newStringPointer = concatStr;
 	if (concatStr == NULL)
-	{
 		return (NULL);
-	}
 	for (; *s1; s1++, newStringPointer++)
-	{
 		*newStringPointer = *s1;
-	}
 	if (n > lens2)
 		for (; *s2; s2++, newStringPointer++)
 			*newStringPointer = *s2;

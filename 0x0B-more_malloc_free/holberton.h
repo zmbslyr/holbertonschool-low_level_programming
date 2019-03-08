732d3eb992aada8c6d9d8eed94ccb6ab444dd14e
zmbslyr <mhedgeland@gmail.com>
2019-03-08 19:26:01 +0000

    Header Update

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -3,3 +3,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _strlen(char *str);
+void simple_print_buffer(char *buffer, unsigned int size);

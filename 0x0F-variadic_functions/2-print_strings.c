4c684d46508ee9963b505cd1181516fb4d5b14c6
Mark Hedgeland <markhedgeland@Marks-iMac.local>
2019-03-16 11:06:06 -0400

    initial commit of print_strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings followed by a new line
+ * @seperator: spaces between words
+ * @n: Number of words to print
+ *
+ * Return: void
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+}

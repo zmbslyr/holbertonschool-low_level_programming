adc347ed80f87370a479fc0536340945305cb553
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-03-15 20:38:10 -0400

    Adds all numbers fed into function

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,27 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * 
- * 
- * 
- * Return:
+ * sum_them_all - Adds up all given numbers
+ * @n: - inputs
+ *
+ * Return: Sum of all numbers given
  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int index;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(valist, n);
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
+}

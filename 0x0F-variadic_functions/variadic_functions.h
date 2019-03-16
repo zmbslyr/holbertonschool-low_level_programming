19749383e252902ca162c87950803e101d206773
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-03-15 20:42:13 -0400

    Header update

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
 #ifndef _VARIADIC_FUNCTIONS_
 #define _VARIADIC_FUNCTIONS_
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif

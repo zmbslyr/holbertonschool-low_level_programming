48f40969868e0e6634dd09ce45b1b0002161befc
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-03-16 15:58:59 -0400

    Prints strings taken into function

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,4 +12,23 @@
  */
 void print_strings(const char *seperator, const unsigned int n, ...)
 {
+	va_list valist;
+	unsigned int index;
+	char *string;
+
+	va_start(valist, n);
+	for (index = 0; index < n; index++)
+	{
+		string = va_arg(valist, char*);
+		if (string == NULL)
+			printf("nil");
+		else
+			printf("%s", string);
+		if (seperator != NULL && index < (n - 1))
+		{
+			printf("%s", seperator);
+		}
+	}
+	va_end(valist);
+	printf("\n");
 }

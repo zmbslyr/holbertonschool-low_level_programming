855dbd159333ec18998fa4884bd6bea703d75558
zmbslyr <mhedgeland@gmail.com>
2019-03-29 23:35:37 +0000

    Clears a bit at the given index

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - Sets the value of bit at index to 0
+ * @n: Integer to change bit in
+ * @index: Index to change
+ *
+ * Return: 1 (Success)
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || index > 63)
+		return (-1);
+	if (*n & 1 << index)
+		*n ^= 1 << index;
+	else
+		*n &= 1 << index;
+	return (1);
+}

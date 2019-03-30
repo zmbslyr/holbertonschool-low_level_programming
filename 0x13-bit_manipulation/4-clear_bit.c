f3b072f87f0cbd02c7a1ea10e1a217ba0795ed20
zmbslyr <mhedgeland@gmail.com>
2019-03-30 01:11:23 +0000

    Batty Fix

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
+
 	if (n == NULL || index > (sizeof(unsigned long int) * 8) - 1)
 		return (-1);
 	mask = ~(1 << index);

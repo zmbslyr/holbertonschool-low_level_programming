86c6524d7d9bbd1c061dca87aef472ab03b946a8
zmbslyr <mhedgeland@gmail.com>
2019-04-02 15:44:18 +0000

    Initial commit

---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* _HOLBERTON_ */

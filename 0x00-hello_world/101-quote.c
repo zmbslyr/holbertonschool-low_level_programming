e3fd6ef14827abf24bda18cf4004cf82d604d5f0
zmbslyr <mhedgeland@gmail.com>
2019-04-13 02:15:23 +0000

    prints quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, quote, strlen(quote));
 	write(2, "\n", 1);

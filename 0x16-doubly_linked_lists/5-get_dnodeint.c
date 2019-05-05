a6ccc098b0bd9204e6a558cf1e088fc488e2431f
zmbslyr <mhedgeland@gmail.com>
2019-05-05 16:02:48 +0000

    Returns a node at an index

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns a node at an index
+ * @head: Start of the linked list
+ * @index: Index to find node at
+ *
+ * Return: Node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+	dlistint_t *nodePointer;
+
+	if (head == NULL)
+		return (NULL);
+	nodePointer = head;
+	for (count = 0; count < index; count++)
+		nodePointer = nodePointer->next;
+	if (nodePointer == NULL)
+		return (NULL);
+	return (nodePointer);
+}

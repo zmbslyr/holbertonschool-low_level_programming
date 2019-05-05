960c79f648f4acc336ab3c9b7f8b9dd29fa84fa6
zmbslyr <mhedgeland@gmail.com>
2019-05-05 14:39:15 +0000

    Spelling fix

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	newNode->next = *head;
 	if (*head != NULL)
-		(**head).prev = new;
+		(**head).prev = newNode;
 	*head = newNode;
 	return (newNode);
 }

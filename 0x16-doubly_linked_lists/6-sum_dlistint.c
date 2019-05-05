d6b5270b8800f43e169f3701fef1587af37ce384
zmbslyr <mhedgeland@gmail.com>
2019-05-05 16:41:44 +0000

    Adds up data in all the nodes

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns the sum of all nodes in list
+ * @head: The start of the linked list
+ *
+ * Return: Sum of all nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *nodePointer;
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	nodePointer = head;
+	while (nodePointer != NULL)
+	{
+		sum += nodePointer->n;
+		nodePointer = nodePointer->next;
+	}
+	return (sum);
+}

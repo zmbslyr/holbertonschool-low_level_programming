a386f3880c2c96750d510033ffb7b0010ffbd694
zmbslyr <mhedgeland@gmail.com>
2019-05-05 14:37:25 +0000

    added extra cases for previous pointer

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
 
+	if (head == NULL)
+		return (NULL);
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
 	newNode->prev = NULL;
 	newNode->next = *head;
+	if (*head != NULL)
+		(**head).prev = new;
 	*head = newNode;
 	return (newNode);
 }

cb28241ffa36e8d0610ca070c340a46235000c23
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-19 18:43:28 -0400

    Counting sort algorithm

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -9,37 +9,37 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int output[size], i = 0, n = size, maxVal = 0, *values, x = 0;
+	int i = 0, maxVal = 0, *values, x = 0, val = 0;
 
-	/* find largest value in array */
+	if (array == NULL || size < 2)
+		return;
+
 	maxVal = array[0];
-	for (i = 1;, i < (int)size; i++)
-		if (maxVal > array[i])
+	for (i = 1; i < (int)size; i++)
+		if (maxVal < array[i])
 			maxVal = array[i];
-
-	/* set size of value array */
+	maxVal++;
 	values = malloc(sizeof(int) * maxVal);
 	if (values == NULL)
 		return;
-
-	/* set value array to all 0 */
 	for (i = 0; i < maxVal; i++)
 		values[i] = 0;
-
-	/* sort into values */
 	for (i = 0; i < (int)size; i++)
 	{
 		val = array[i];
 		values[val]++;
 	}
-
-	for (i = 0; i < (int)size; i++, x++)
-		if (values[x] > 0)
-			while (values[x] != 0)
-			{
-				output[i] = x;
-				i++;
-			}
-
-
+	for (i = 1; i < maxVal; i++)
+		values[i] += values[i - 1];
+	print_array(values, maxVal);
+	for (i = maxVal - 1; i > 0; i--)
+		values[i] -= values[i - 1];
+	for (i = 0; i < (int)size; x++)
+		while (values[x] > 0)
+		{
+			array[i] = x;
+			values[x]--;
+			i++;
+		}
+	free(values);
 }

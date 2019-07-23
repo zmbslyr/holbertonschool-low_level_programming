981b392d18b0553120d4c4b03ef74a7e635ac7d5
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-23 16:55:01 -0400

    [GLOBAL][c] Finds if node is root in binary tree

---
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Finds if a given node is a root in a binary tree
+ * @node: Node to check
+ *
+ * Return: 1 if node is root else 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if ((node->left != NULL) & (node->right != NULL))
+		return (1);
+	return (0);
+}

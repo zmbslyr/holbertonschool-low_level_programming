6a137f7150b5f5763c618380b4123f43c9afe879
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-23 17:49:39 -0400

    [GLOBAL][c] Finds the number of leaves on a binary tree

---
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts number of leaves in a binary tree
+ * @tree: root node pointer
+ * Return: returns num of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL &&)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}

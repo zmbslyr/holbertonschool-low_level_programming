a5e99c32b73e6115921db5d8776104784f00e702
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-25 00:15:10 -0400

    [GLOBAL][c] Betty fixes

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -7,21 +7,21 @@
  *
  * Return: New node (Success) | NULL (Failure)
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
 	if (parent == NULL)
 		return (NULL);
-	newNode = malloc(sizeof(binary_tree_t));
+	newNode = binary_tree_node(parent, value);
 	if (newNode == NULL)
 		return (NULL);
 	if (parent->left != NULL)
-		free(parent->left);
-	newNode->parent = parent;
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
+	{
+		newNode->left = parent->left;
+		newNode->left->parent = newNode;
+	}
 	parent->left = newNode;
 	return (newNode);
 }
--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Insert node on right branch
- * @parent: Parent node of the inserted node
- * @value: Value to put in new node
- *
- * Return: Pointer to new node (Success) | NULL (Failure)
+ * binary_tree_insert_right - inserts a node to the right in tree
+ * @parent: pointer to parent node
+ * @value: vlaue of new node
+ * Return: returns new node or NULL
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *rightNode;
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		rightNode->right = parent->right;
-		parent->right = rightNode;
+		rightNode->right->parent = rightNode;
 	}
 	parent->right = rightNode;
 	return (rightNode);

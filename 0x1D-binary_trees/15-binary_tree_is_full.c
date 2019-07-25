3f34c2a67d249c83991b8441ee10d5769ed753ba
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-25 00:28:02 -0400

    [GLOBAL][c] Checks if a binary tree is full

---
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - Checks if a node has two children or no children
+ * @tree: Tree to check
+ *
+ * Return: 1 if tree is full else 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if ((tree->left != NULL && tree->right == NULL) ||
+		(tree->right != NULL && tree->left == NULL))
+		return (0);
+	if (tree->left->left == NULL && tree->right->right == NULL)
+		return (1);
+	return (0);
+}

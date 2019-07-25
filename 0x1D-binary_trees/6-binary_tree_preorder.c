f3cad520194044bf5db2f5b22bf68bc30cd0aa4b
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-25 00:18:52 -0400

    [GLOBAL][c] Betty fixes

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -5,15 +5,13 @@
  * @tree: tree to traverse
  * @func: function for callpack and to print
  */
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL)
-        return;
-    if (func == NULL)
-        return;
-    
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	if (tree == NULL)
+		return;
+	if (func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }

f7aea3d385d896a364404b732f39a861a7347352
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-26 16:13:36 -0400

    [GLOBAL][c] Removed one conditional statement

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -16,7 +16,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->right != NULL && tree->left == NULL)
 		return (0);
-	if (tree->left->left == NULL && tree->right->right == NULL)
-		return (1);
 	return (0);
 }

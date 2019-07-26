494e8b209c3b9b9fbec65b1ad5ad9fa789867cae
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-26 16:06:23 -0400

    [GLOBAL][c] Added condition to check if 'node->parent' is NULL

--- 0x1D-binary_trees/5-binary_tree_is_root.c
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -10,6 +10,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
+	if (node->parent == NULL)
+		return (1);
 	if ((node->left != NULL) && (node->right != NULL))
 		return (1);
 	return (0);

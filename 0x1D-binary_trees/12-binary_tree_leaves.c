72a4d7422309c83a6679c9ef205ab195dbd64459
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-26 15:26:44 -0400

    [GLOBAL][c] Added new condition

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -10,7 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL && tree->left == NULL &&)
+	if (tree->right == NULL && tree->left == NULL)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }

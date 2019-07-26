89ed13d51f651d5245bcafcaddc5d66f10aad996
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-07-26 16:17:51 -0400

    [GLOBAL][c] Added new fail condition

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);

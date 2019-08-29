f852c934c4474b5b4a156d8f43633c2849460eed
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-29 19:26:21 -0400

    [GLOBAL][c] Header update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,13 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/* Includes */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_part(int const *first, int const *last);
 
 #endif /* SEARCH_ALGOS_H */
